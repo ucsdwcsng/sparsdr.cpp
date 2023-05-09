aeb381275252b25446d17a18f3d1c8d221033908
Raghav Subbaraman <rsubbaraman@eng.ucsd.edu>
2023-05-09 12:34:37 -0700

    set up CMakeLists

---
+++ src/sparsdr.cpp
@@ -0,0 +1,11 @@
+#include <sparsdr.h>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
---
+++ src/sparsdr.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace sparsdrproc
+{
+    int add(int a, int b);
+    int multiply(int a, int b);
+}
